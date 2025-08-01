37dc5ea2ac67eba85ac04671161e87d60b6de111
Gómez Leonardo <gomezleo35@gmail.com>
2025-08-01 15:02:02 -0300

    no-argument constructor is added

--- src/main/java/com/meli/item/model/Product.java
+++ src/main/java/com/meli/item/model/Product.java
@@ -2,10 +2,12 @@ package com.meli.item.model;
 import lombok.Setter;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Product {
     private String id;
     private String name;
